deb2f36b9b322788d06707dd4841bada5b32c5ec
pablo222017 <pablo222005@gmail.com>
2019-09-06 16:42:52 -0300

    TP01

--- TP01/Funciones.c
+++ TP01/Funciones.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+void menu()
+{
+    printf("                     ¡¡¡ Bienvenido a su calculadora !!!               ");
+    printf("                                                                       ");
+    printf("\n         ************************************************************");
+    printf("\n         *    1.- Suma                      4.- Division            *");
+    printf("\n         *                                                          *");
+    printf("\n         *    2.- Resta                     5.- Factorial           *");
+    printf("\n         *                                                          *");
+	printf("\n         *    3.- Multiplicacion            6.- Salir               *");
+	printf("\n         ************************************************************");
+	printf("\n                   Nota: No es posible dividir por cero              ");
+	printf("\n                                                                     ");
+}
 float sumar(float a,float b)
 {
     float resultado;
@@ -23,14 +38,13 @@ float dividir(float a,float b)
     resultado=a/b;
     return resultado;
 }
-double factorizar(double numFactorial)
+double factorizar(float a)
 {
-    double resultadoFactorial;
-    int numero=0;
-    while (numero>0)
+    double factorial=1;
+    while (a>0)
         {
-        resultadoFactorial=(double)numFactorial*numero;
-        numero--;
+        factorial=factorial*a;
+        a--;
         }
-    return resultadoFactorial;
+        return factorial;
 }
--- TP01/main.c
+++ TP01/main.c
@@ -1,32 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <conio.h>
 #include "Prototipos.h"
+
 int main()
 {
-    int num1,num2;
-    float resultado;
+    float num1,num2,resultado;
     double factorA,factorB;
     char opcion;
-    printf("        ¡¡¡ Bienvenido a su calculadora !!!    \n");
-    printf("                                               \n");
     printf("Para comenzar, ingrese primero los operandos.  \n");
     printf("\nIngrese primer numero: ");
-    scanf("%d",&num1);
+    scanf("%f",&num1);
     printf("\nIngrese segundo numero: ");
-    scanf("%d",&num2);
-    system("cls");
-    printf("\n         ************************************************************");
-    printf("\n         *    1.- Suma                      4.- Division            *");
-    printf("\n         *                                                          *");
-    printf("\n         *    2.- Resta                     5.- Factorial           *");
-    printf("\n         *                                                          *");
-	printf("\n         *    3.- Multiplicacion            6.- Salir               *");
-	printf("\n         ************************************************************");
-	printf("\n                   Nota: No es posible dividir por cero              ");
-	printf("\n                                                                     ");
-	printf("\n                 Los operandos ingresados son: A=%d  y B=%d       \n ",num1,num2);
-    while(opcion!='6')
+    scanf("%f",&num2);
+	system("cls");
+	menu();
+	printf("\nLos operandos ingresados son: A=%.3f  y B=%.3f   \n ",num1,num2);
+    do
         {
             printf("\nIngrese la opcion correspondiente a la operacion deseada: ");
             fflush(stdin);
@@ -35,15 +24,15 @@ int main()
             {
             case '1':
             resultado=sumar(num1,num2);
-            printf("\nEl resultado de %d + %d es: %.2f\n",num1,num2,resultado);
+            printf("\nEl resultado de A+B es: %.3f\n",resultado);
             break;
             case '2':
             resultado=restar(num1,num2);
-            printf("\nEl resultado de %d - %d es: %.2f\n",num1,num2,resultado);
+            printf("\nEl resultado de A-B es: %.3f\n",resultado);
             break;
             case '3':
             resultado=multiplicar(num1,num2);
-            printf("\nEl resultado de %d * %d es: %.2f\n",num1,num2,resultado);
+            printf("\nEl resultado de A*B es: %.3f\n",resultado);
             break;
             case '4':
             if(num1<1 || num2<1)
@@ -52,19 +41,21 @@ int main()
                 }else
                 {
                     resultado=dividir(num1,num2);
-                    printf("\nEl resultado de %d / %d es: %.2f\n",num1,num2,resultado);
+                    printf("\nEl resultado de A/B es: %.3f\n",resultado);
                 }
             break;
             case '5':
             factorA=factorizar(num1);
             factorB=factorizar(num2);
-            printf("\nEl factorial A!=%d y B!=%d \n",factorA,factorB);
+            printf("\nEl factorial de A!=%e\nEl factorial de B!=%e\n",factorA,factorB);
+            break;
+            case '6':
+            printf("\nUsted eligio salir");
             break;
             default:
-            printf("\nUsted eligio salir\n");
+            printf("\nElija una opcion valida\n");
             break;
             }
-        }
-
+        }while(opcion!='6');
     return 0;
 }
